6731a2a4e7a2b083844c4b19f404f7c62b2cae2b
Bing <wangbing@hcjian.cn>
2024-10-22 22:22:59 +0800

    bugfix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import api_stream_base_analysis, api_base_analysis, api_bazi_need, api_career_analysis, api_dayun_analysis, api_love_analysis, api_xiji_analysis
+from routers import api_base_analysis, api_bazi_need, api_career_analysis, api_dayun_analysis, api_love_analysis, api_xiji_analysis
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
@@ -28,7 +28,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 async def root():
     return FileResponse("static/test_stream.html")
 
-app.include_router(api_stream_base_analysis.router)
 app.include_router(api_base_analysis.router)
 app.include_router(api_career_analysis.router)
 app.include_router(api_dayun_analysis.router)
